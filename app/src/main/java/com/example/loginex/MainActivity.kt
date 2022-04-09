5808cf061a9bd3d8fcfa235524ad85b29d36f264
flex2020 <hosan_1@naver.com>
2022-04-09 22:11:40 +0900

    22.04.09

--- app/src/main/java/com/example/loginex/MainActivity.kt
+++ app/src/main/java/com/example/loginex/MainActivity.kt
@@ -10,8 +10,8 @@ import com.example.loginex.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     // 뷰 바인딩 객체 선언
     private lateinit var binding: ActivityMainBinding
-    private var id: String? = null
-    private var pwd: String? = null
+    private val admin_ID: String = "admin"
+    private val admin_PWD: String = "1234"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +21,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.registerButton.setOnClickListener {
-            Toast.makeText(this@MainActivity,"회원가입으로 이동합니다.", Toast.LENGTH_SHORT).show()
-            goReg()
+            Toast.makeText(this@MainActivity,"회원가입 화면으로 이동합니다.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+        binding.loginButton.setOnClickListener {
+            if(binding.editTextId.text.toString().equals(admin_ID) && binding.editTextPw.text.toString().equals(admin_PWD)) {
+                Toast.makeText(this@MainActivity, "관리자님, 어서오세요!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, afterLogin::class.java)
+                startActivity(intent)
+            }
+            else {
+                Toast.makeText(this@MainActivity, "아이디 또는 비밀번호가 틀렸습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
 
-    }
-
-    fun goReg() {
-        val intent = Intent(this, Register::class.java)
-        startActivity(intent)
     }
 
 
---
+++ app/src/main/java/com/example/loginex/afterLogin.kt
@@ -0,0 +1,11 @@
+package com.example.loginex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class afterLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_login)
+    }
+}
